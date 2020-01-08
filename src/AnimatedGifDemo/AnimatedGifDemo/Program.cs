4790f6f6182365eeba911624fc6f2c5e869be306
Wagsn <wagsn@foxmail.com>
2020-01-08 21:21:09 +0800

    Add AnimatedGifDemo

---
+++ src/AnimatedGifDemo/AnimatedGifDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using AnimatedGif;
+using System.Drawing;
+using System;
+using System.Linq;
+
+namespace AnimatedGifDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var imagesPath = System.IO.Path.Combine(AppContext.BaseDirectory, "Images");
+            if (!System.IO.Directory.Exists(imagesPath)) return;
+            var files = System.IO.Directory.EnumerateFiles(imagesPath, "*.jpg");  // png
+            if (!files.Any()) return;
+            // 33ms delay (~30fps 帧每秒) 
+            using (var gif = AnimatedGif.AnimatedGif.Create("gif.gif", 1000))
+            {
+                foreach(var path in files)
+                {
+                    var img = Image.FromFile(path); 
+                    gif.AddFrame(img, delay: -1, quality: GifQuality.Default);
+                }
+            }
+        }
+    }
+}
