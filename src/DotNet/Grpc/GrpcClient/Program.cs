ef7ba4d79444833cd5a6065aa429e66d534dcd08
Wangsen <wagsn@foxmail.com>
2021-09-27 09:27:01 +0800

    Grpc调整

--- src/DotNet/Grpc/GrpcClient/Program.cs
+++ src/DotNet/Grpc/GrpcClient/Program.cs
@@ -18,8 +18,7 @@ namespace GrpcClient
             // The port number(5001) must match the port of the gRPC server.
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(
-                              new HelloRequest { Name = "GreeterClient" });
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting: " + reply.Message);
 
             /*
