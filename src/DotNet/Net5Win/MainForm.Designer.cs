889ed06f27bace12a3206218a01e0a3c47295d3b
Wangsen <wagsn@foxmail.com>
2021-09-29 18:25:18 +0800

    提交

--- src/DotNet/Net5Win/MainForm.Designer.cs
+++ src/DotNet/Net5Win/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Net5Win
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -32,7 +32,7 @@ namespace Net5Win
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "MainForm";
         }
 
         #endregion
--- src/DotNet/Net5Win/Program.cs
+++ src/DotNet/Net5Win/Program.cs
@@ -17,7 +17,7 @@ namespace Net5Win
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
